69bed71ef03feb2ccc3a91d7689a2354c966854f
AshuMishraG <aashutoshm77@gmail.com>
2025-02-12 22:32:06 +0530

    Day-3

---
+++ Repetitions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+using ll = long long;
+#define ff first
+#define ss second
+
+int main() {
+  string s;
+  cin >> s;
+
+  char x = s[0];
+  int c = 1;
+  int count = 1;
+
+  for (int i = 1; i < s.size(); i++) {
+    if (s[i] != x) {
+      c = 0;
+      x = s[i];
+    }
+    c++;
+    count = max(count, c);
+  }
+  cout << count << endl;
+
+  return 0;
+}
